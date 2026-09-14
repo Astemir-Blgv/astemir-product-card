94294c96f1ac415f0d0db93aca644ee5782cd053
Astemir-Blgv <astemir0007@gmai.com>
2026-09-14 20:40:38 +0300

    fixed errors

--- homework-8.js
+++ homework-8.js
@@ -1,109 +1,104 @@
-//3 . Объект личная информация 
+//3 . Объект личная информация
 const person = {
-  firstName : `Астемир`,
-  lastName  : `Балагов`,
-  email : `b.astemir07@mail.ru`,
-  job   : `Инженер строитель`,
-  age : `35`,
-  country : `Russia`,
-  city : `Nalchik`,
-  relationshipStatus : `Не женат`,
-  hobby:`Горы`,
-}
+  firstName: "Астемир",
+  lastName: "Балагов",
+  email: "b.astemir07@mail.ru",
+  job: "Инженер строитель",
+  age: "35",
+  country: "Russia",
+  city: "Nalchik",
+  relationshipStatus: "Не женат",
+  hobby: "Горы",
+};
 
 //4. Объект , хранящие данные об автомобиле.
 const car = {
-  brand : `BMW`,
-  model : `530d`,
-  year  : `2015`,
-  color : `black`,
-  transmition : `Auto`,
-car.owner = person ; 
-}
+  brand: "BMW",
+  model: "530d",
+  year: "2015",
+  color: "black",
+  transmition: "Auto",
+};
 
+car.owner = person;
 //6.Фунцкия добваление свойства, максимальная скорость.
-function ensureMaxSpeed (obj) {
-  if
-  (!obj.hasOwnProperty (`maxSpeed`)) {
-    obj [`maxSpeed`] = 220;
+function ensureMaxSpeed(obj) {
+  if (!obj.hasOwnProperty("maxSpeed")) {
+    obj["maxSpeed"] = 220;
   }
 }
-ensureMaxSpeed (car);
-console.log (`car после добваления maxSpeed:`, car)
+ensureMaxSpeed(car);
+console.log("car после добваления maxSpeed:", car);
 
 //Задание 6.  Функция вывода значения свойства объектов.
-function getPropertyValue(obj,prop) {
-  console.log (`Значение свойства"${prop}":`,obj[prop]);
+function getPropertyValue(obj, prop) {
+  console.log('Значение свойства"${prop}":', obj[prop]);
 }
-getPropertyValue(car,`brand`)
+console.log(car, "brand");
 
-//7. Массив продукты 
-const products = [`хлеб,молоко','яйца','масло','сыр'];
-
+//7. Массив продукты
+const products = ["хлеб", "молоко", "яйца", "масло", "сыр"];
 
 //8. Массив книги.
 const books = [
   {
-    title: 'Война и мир',
-    author: 'Лев Толстой',
+    title: "Война и мир",
+    author: "Лев Толстой",
     year: 1869,
-    coverColor: 'красный',
-    genre: 'роман'
+    coverColor: "красный",
+    genre: "роман",
   },
   {
-    title: 'Преступление и наказание',
-    author: 'Фёдор Достоевский',
+    title: "Преступление и наказание",
+    author: "Фёдор Достоевский",
     year: 1866,
-    coverColor: 'серый',
-    genre: 'роман'
+    coverColor: "серый",
+    genre: "роман",
   },
   {
-    title: 'Тихий Дон',
-    author: 'Михаил Шолохов',
+    title: "Тихий Дон",
+    author: "Михаил Шолохов",
     year: 1940,
-    coverColor: 'фиолетовый',
-    genre: 'роман'
-  }
+    coverColor: "фиолетовый",
+    genre: "роман",
+  },
 ];
 books.push({
-  title: 'Дубровский',
-  author: 'Александр Пушкин',
+  title: "Дубровский",
+  author: "Александр Пушкин",
   year: 1832,
-  coverColor: 'синий',
-  genre: 'роман'
+  coverColor: "синий",
+  genre: "роман",
 });
-console.log('Массив книг после добавления:', books);
+console.log("Массив книг после добавления:", books);
 
 // Задание 9. Ещё один массив книг + объединение с предыдущим
 const otherBooks = [
   {
-    title: 'Мёртвые души',
-    author: 'Николай Гоголь',
+    title: "Мёртвые души",
+    author: "Николай Гоголь",
     year: 1835,
-    coverColor: 'красный',
-    genre: 'повесть'
+    coverColor: "красный",
+    genre: "повесть",
   },
   {
-    title: 'Муму',
-    author: 'Иван Тургенев',
+    title: "Муму",
+    author: "Иван Тургенев",
     year: 1852,
-    coverColor: 'синий',
-    genre: 'рассказ'
-  }
+    coverColor: "синий",
+    genre: "рассказ",
+  },
 ];
 const allBooks = [...books, ...otherBooks];
-console.log('Объединённый массив книг:', allBooks);
+console.log("Объединённый массив книг:", allBooks);
 
 // Задание 10. Функция с map
 
 function markRareBooks(booksArray) {
-  return booksArray.map(book => ({
+  return booksArray.map((book) => ({
     ...book,
-    isRare: book.year > 1850
+    isRare: book.year > 1850,
   }));
 }
 const rareMarkedBooks = markRareBooks(allBooks);
-console.log('Книги со свойством isRare:', rareMarkedBooks);
-
-
- 
+console.log("Книги со свойством isRare:", rareMarkedBooks);
